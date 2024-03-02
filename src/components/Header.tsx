031f9ae2b78203329e789ce49337adc51861161c
unknown <guivieirasilva02@gmail.com>
2024-03-02 18:42:16 -0300

    componente de Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export function Header() {
+  return (
+    <header className="flex w-full max-w-[1280px] items-center justify-between border border-b-base-background px-10 py-3">
+      <strong>CNAE</strong>
+      <button className="rounded-md bg-base-list p-2">
+        <img src="./web.svg" alt="" />
+      </button>
+    </header>
+  )
+}
