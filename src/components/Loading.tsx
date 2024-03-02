175cbcb88caf2f8ef0e01c059d6679269826a481
unknown <guivieirasilva02@gmail.com>
2024-03-02 19:03:05 -0300

    component de loading

---
+++ src/components/Loading.tsx
@@ -0,0 +1,7 @@
+export function Loading() {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <div className="h-16 w-16 animate-spin rounded-full border-b-2 border-t-2 border-gray-900"></div>
+    </div>
+  )
+}
