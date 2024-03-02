8a4ac461380f99d1f9e18c116e35d22b04accdc6
unknown <guivieirasilva02@gmail.com>
2024-03-02 18:35:00 -0300

    salvando cores e fonte

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'base-background': '#E5E8EB',
+        'base-list': '#E8EDF2',
+        'base-span': '#4F7396',
+      },
+    },
+    fontFamily: {
+      sans: ['Public Sans', 'sans-serif'],
+    },
   },
   plugins: [],
 }
-
